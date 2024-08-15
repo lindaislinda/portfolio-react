8ea82614a43b5a66204472896c6aa7f782e174be
lindaislinda <thaonguyenntp2003@gmail.com>
2024-08-15 05:59:40 -0500

    Update resume link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import Resume from '../../assets/Linda-Resume.pdf'
-import {FiDownload} from 'react-icons/fi'
+import React from "react";
+import Resume from "../../assets/Linda-Resume.pdf";
+import { FiDownload } from "react-icons/fi";
 
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={Resume} download className='btn'><FiDownload/>  Resume</a>
-        <a href="#contact" className='btn btn-primary'>Let's Connect!</a>
+      <a
+        href="https://drive.google.com/file/d/1nrvX4cce67lgjgACHIoKlR0QdKlqiTl-/view?usp=sharing"
+        download
+        className="btn"
+        target="_blank"
+      >
+        <FiDownload /> Resume
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Connect!
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
